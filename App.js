2f5a36cc37dcddc0fa5067e860d29eed2fc67b5b
bollinca <bollinca522@gmail.com>
2019-12-19 12:17:49 -0500

    Add some preliminary styling, create BookCards

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import BookCard from './BookCard/BookCard.js'
+
 function App() {
   return (
     <div className="App">
       <header>
         <h1>Welcome to Your Library!</h1>
+        <BookCard />
+        <BookCard />
       </header>
     </div>
   );
---
+++ BookCard/BookCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './BookCard.css';
+
+const bookCard = (props) => {
+  return (
+    <div className='BookCard'>
+      This is a div
+    </div>
+  )
+}
+
+export default bookCard
