ace8f189d87f0f2b736f47f9f61571bac5f5e6ae
bollinca <bollinca522@gmail.com>
2019-12-20 09:55:08 -0500

    Create book props and use for display

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import BookCard from './BookCard/BookCard.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>Welcome to Your Library!</h1>
-        <BookCard />
-        <BookCard />
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    books: [
+      {title: "test",
+       author: 'Mr. Author',
+       pages: '142',
+       readStatus: 'True'}
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Welcome to Your Library!</h1>
+          <BookCard
+            title={this.state.books[0].title}
+            author={this.state.books[0].author}
+            pages={this.state.books[0].pages}
+            readStatus={this.state.books[0].readStatus} />
+          <BookCard />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
