d1fe993827b5d1be9c017e674323ff3c39b305a8
bollinca <bollinca522@gmail.com>
2019-12-21 11:42:33 -0500

    Edit form layout, add appropriate properties to inputs

--- NewItemForm/NewItemForm.js
+++ NewItemForm/NewItemForm.js
@@ -5,10 +5,15 @@ const newItemForm = () => {
   return (
     <form className='NewItemForm'>
       <button>EXIT</button>
-      TITLE:<input></input>
-      AUTHOR:<input></input>
-      PAGES:<input></input>
-      STATUS TOGGLE
+      <div>
+        <input className='textInput' type='text' placeholder='Enter Title'></input>
+        <input className='textInput' type='text' placeholder='Enter Author'></input>
+        <input className='textInput' type='text' placeholder='Enter # of pages'></input>
+      </div>
+      <div>
+        <input type='radio' name='read-status' value='read' />Read
+        <input type='radio' name='read-status' value='unread' />Unread
+      </div>
       <button>SUBMIT</button>
     </form>
   )
