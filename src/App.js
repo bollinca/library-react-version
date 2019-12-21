d302a54c6efe420278e92b8e8baf4fccb62d52dc
bollinca <bollinca522@gmail.com>
2019-12-21 11:12:49 -0500

    Output book list dynamically

--- src/App.js
+++ src/App.js
@@ -6,24 +6,45 @@ import BookCard from './BookCard/BookCard.js'
 class App extends Component {
   state = {
     books: [
-      {title: "test",
-       author: 'Mr. Author',
-       pages: '142',
-       readStatus: 'True'}
+      {
+        title: "test",
+        author: 'Mr. Author',
+        pages: '142',
+        readStatus: 'True'
+      },
+      {
+        title: "Test Number 2",
+        author: 'Mrs. Author',
+        pages: '155',
+        readStatus: 'True'
+      },
     ]
   }
 
   render() {
+
+    let booksList = null;
+
+    if (this.state.books) {
+      booksList = (
+        this.state.books.map((book, index) => {
+          return <BookCard
+            title={this.state.books[index].title}
+            author={this.state.books[index].author}
+            pages={this.state.books[index].pages}
+            readStatus={this.state.books[index].readStatus}
+          />
+        })
+      )
+    }
+
     return (
       <div className="App">
         <header>
           <h1>Welcome to Your Library!</h1>
-          <BookCard
-            title={this.state.books[0].title}
-            author={this.state.books[0].author}
-            pages={this.state.books[0].pages}
-            readStatus={this.state.books[0].readStatus} />
-          <BookCard />
+          <div>
+            {booksList}
+          </div>
         </header>
       </div>
     );
