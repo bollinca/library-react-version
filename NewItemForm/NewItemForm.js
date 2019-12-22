5048874a13b6c417411a44a391fdbb75360997de
bollinca <bollinca522@gmail.com>
2019-12-22 09:53:41 -0500

    Connect form display to form state

--- NewItemForm/NewItemForm.js
+++ NewItemForm/NewItemForm.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import './NewItemForm.css'
 
-const newItemForm = () => {
+const newItemForm = (props) => {
   return (
     <form className='NewItemForm'>
       <button>EXIT</button>
       <div>
-        <input className='textInput' type='text' placeholder='Enter Title'></input>
-        <input className='textInput' type='text' placeholder='Enter Author'></input>
-        <input className='textInput' type='text' placeholder='Enter # of pages'></input>
+        <input
+          className='textInput'
+          type='text'
+          placeholder='Enter Title'
+          value={props.title}
+        >
+        </input>
+
+        <input
+          className='textInput'
+          type='text'
+          placeholder='Enter Author'
+          value={props.author}
+        ></input>
+
+        <input
+          className='textInput'
+          type='text'
+          placeholder='Enter # of pages'
+          value={props.pages}
+        ></input>
+
       </div>
       <div>
         <input type='radio' name='read-status' value='read' />Read
