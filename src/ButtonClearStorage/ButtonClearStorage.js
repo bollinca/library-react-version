db5e2364c414bcc3673d9d1748016d755c68b3a4
bollinca <bollinca522@gmail.com>
2019-12-22 09:39:43 -0500

    Add new book and clear storage buttons

---
+++ src/ButtonClearStorage/ButtonClearStorage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const buttonClearStorage = (props) => {
+  const styleConst = {
+    display: 'block',
+    fontSize: '20px',
+    margin: '20px auto',
+    padding: '6px',
+  }
+  return (
+    <button style={styleConst}>Clear Storage</button>
+  )
+}
+
+export default buttonClearStorage;
