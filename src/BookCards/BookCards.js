eb3cbedb7d1fac2b8b1b92e39731c3614c4c578c
bollinca <bollinca522@gmail.com>
2020-01-01 13:21:37 -0500

    Add book delete button (non-functional), factor out BookCards

---
+++ src/BookCards/BookCards.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import BookCard from './BookCard/BookCard';
+
+const bookCards = (props) => {
+
+  let readStatusDisplay = 'False';
+
+  return (props.books.map((book, index) => {
+    book.readStatus === true ? readStatusDisplay = 'True' : readStatusDisplay = 'False';
+    return <BookCard
+      title={book.title}
+      key={book.title}
+      author={book.author}
+      pages={book.pages}
+      readStatus={readStatusDisplay}
+    />
+  }))
+}
+export default bookCards
