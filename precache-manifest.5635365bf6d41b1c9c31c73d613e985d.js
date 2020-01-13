62dea320e2ad273d5558ba4911c4b81961945b9b
bollinca <bollinca522@gmail.com>
2020-01-13 14:01:51 -0500

    Updates

---
+++ precache-manifest.5635365bf6d41b1c9c31c73d613e985d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c48464851f2c6736692b4280d056a8",
+    "url": "/library-react-version/index.html"
+  },
+  {
+    "revision": "52398aff39ab97b83b07",
+    "url": "/library-react-version/static/css/main.629e1464.chunk.css"
+  },
+  {
+    "revision": "579633a60a1a58570272",
+    "url": "/library-react-version/static/js/2.e59e6264.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/library-react-version/static/js/2.e59e6264.chunk.js.LICENSE"
+  },
+  {
+    "revision": "52398aff39ab97b83b07",
+    "url": "/library-react-version/static/js/main.df33be16.chunk.js"
+  },
+  {
+    "revision": "ec47328900d46f147176",
+    "url": "/library-react-version/static/js/runtime-main.6dc1d919.js"
+  }
+]);
